01a4bbf820e698e1e5552c850215afc5af16d996
Ash Karapetyan <ashot.karapetyan.y@gmail.com>
2023-02-06 22:14:35 +0300

    Fix props

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,6 +6,8 @@ import {
 import { IUser, User } from './interfaces/users.interface';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as crypto from 'crypto';
+import { IdParamDto } from '../common/id-param.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
 
 @Injectable()
 export class UsersService {
@@ -14,7 +16,7 @@ export class UsersService {
     return this.users;
   }
 
-  findOne(params): IUser {
+  findOne(params: IdParamDto): IUser {
     const user = this.users.find((user) => user.id === params.id);
     if (!user) {
       throw new NotFoundException('No user with this id');
@@ -34,17 +36,23 @@ export class UsersService {
     return user;
   }
 
-  update(params, body): IUser {
+  update(params: IdParamDto, body: UpdatePasswordDto): IUser {
     const user = this.findOne(params);
     if (user.password !== body.oldPassword) {
       throw new ForbiddenException('Old password is incorrect');
     }
     const userIndex = this.users.findIndex((user) => user.id === params.id);
-    this.users.splice(userIndex, 1, { ...user, password: body.newPassword });
-    return user;
+    const changedUser = {
+      ...user,
+      password: body.newPassword,
+      version: ++user.version,
+      updatedAt: 1,
+    };
+    this.users.splice(userIndex, 1, changedUser);
+    return changedUser;
   }
 
-  delete(params): void {
+  delete(params: IdParamDto): void {
     const userIndex = this.users.findIndex((user) => user.id === params.id);
     if (userIndex === -1) {
       throw new NotFoundException('No user with this id');
