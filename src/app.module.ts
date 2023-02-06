aecb9dc7657cc5696ccc4cd443b5a5dcae279492
Ash Karapetyan <ashot.karapetyan.y@gmail.com>
2023-02-06 22:11:09 +0300

    Added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [ConfigModule.forRoot(), UsersModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,9 @@ async function bootstrap() {
     }),
   );
 
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+
   const config = new DocumentBuilder()
     .setTitle('Library service')
     .setDescription('The library API description')
@@ -19,6 +23,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
