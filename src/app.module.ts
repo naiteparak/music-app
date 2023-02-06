f56e8fdf2b2c2653adf0d58bc50993bac94645da
Ash Karapetyan <ashot.karapetyan.y@gmail.com>
2023-02-07 02:08:45 +0300

    Add tracks and favorites modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ArtistsModule } from './artists/artists.module';
 import { AlbumsModule } from './albums/albums.module';
+import { TracksModule } from './tracks/tracks.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, ArtistsModule, AlbumsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    ArtistsModule,
+    AlbumsModule,
+    TracksModule,
+    FavoritesModule,
+  ],
   controllers: [],
   providers: [],
 })
