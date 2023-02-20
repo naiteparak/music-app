6ed04f046c1b1e2a3c58a0f5fd3a7c280e3d780d
Ash Karapetyan <ashot.karapetyan.y@gmail.com>
2023-02-20 16:10:00 +0300

    Fix users issue

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,11 +12,11 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { IUser } from './interfaces/users.interface';
 import { CreateUserDto } from './dto/create-user.dto';
 import { IdParamDto } from '../common/id-param.dto';
 import { UpdatePasswordDto } from './dto/update-password.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { UserEntity } from './entities/user.entity';
 
 @ApiTags('Users')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -25,31 +25,31 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get()
-  async findAll(): Promise<IUser[]> {
-    return this.usersService.findAll();
+  async findAll(): Promise<UserEntity[]> {
+    return await this.usersService.findAll();
   }
 
   @Get(':id')
-  async findOne(@Param() params: IdParamDto): Promise<IUser> {
-    return this.usersService.findOne(params);
+  async findOne(@Param() params: IdParamDto): Promise<UserEntity> {
+    return await this.usersService.findOne(params);
   }
 
   @Post()
-  async create(@Body() body: CreateUserDto): Promise<IUser> {
-    return this.usersService.create(body);
+  async create(@Body() body: CreateUserDto): Promise<UserEntity> {
+    return await this.usersService.create(body);
   }
 
   @Put(':id')
   async update(
     @Param() params: IdParamDto,
     @Body() body: UpdatePasswordDto,
-  ): Promise<IUser> {
-    return this.usersService.update(params, body);
+  ): Promise<UserEntity> {
+    return await this.usersService.update(params, body);
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
   async delete(@Param() params: IdParamDto): Promise<void> {
-    return this.usersService.delete(params);
+    return await this.usersService.delete(params);
   }
 }
